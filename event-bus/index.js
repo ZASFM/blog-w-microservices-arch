2e2124c6b1b703e61ea69d60d9f86db57ef9f767
shafi <shafi.bahrami.2015@gmail.com>
2023-06-22 13:57:00 +0200

    event bus event storage

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,8 +5,12 @@ const app=express();
 
 app.use(express.json());
 
+let events=[];
+
 app.post('/events',async (req,res)=>{
    const event=req.body; 
+
+   events.push(event);
 
    //post service
    await axios.post('http://localhost:4000/events',event).catch(err=>console.log(err));
@@ -18,6 +22,10 @@ app.post('/events',async (req,res)=>{
    await axios.post('http://localhost:4003/events',event).catch(err=>console.log(err));
 
    res.status(200).send('ok')
+})
+
+app.get('/events',(req,res)=>{
+   res.send(events);
 })
 
 app.listen(4005,()=>{
